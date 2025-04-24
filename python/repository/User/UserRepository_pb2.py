65c0553a22862212fee2fff71bcc0e66f8d39ea0
André Reis <bakugan.erdna@gmail.com>
2025-04-24 14:30:54 +0100

    nada funciona

--- python/repository/User/UserRepository_pb2.py
+++ python/repository/User/UserRepository_pb2.py
@@ -11,8 +11,8 @@ from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
 _runtime_version.ValidateProtobufRuntimeVersion(
     _runtime_version.Domain.PUBLIC,
-    5,
-    29,
+    6,
+    31,
     0,
     '',
     'repository/UserRepository.proto'
