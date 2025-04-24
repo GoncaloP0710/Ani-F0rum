e0bf29724b7897ec380788b5543d3e130bd55dfa
André Reis <bakugan.erdna@gmail.com>
2025-04-24 15:32:22 +0100

    problema de versoes fixed

--- python/repository/User/UserRepository_pb2.py
+++ python/repository/User/UserRepository_pb2.py
@@ -11,8 +11,8 @@ from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
 _runtime_version.ValidateProtobufRuntimeVersion(
     _runtime_version.Domain.PUBLIC,
-    6,
-    31,
+    5,
+    29,
     0,
     '',
     'repository/UserRepository.proto'
